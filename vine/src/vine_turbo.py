f2994c27c3e07a31814b0ec55e74b06f1588b258
jessieweneedtocook <J.Smith73@newcastle.ac.uk>
2025-04-15 13:42:32 +0100

    initial commit

--- vine/src/vine_turbo.py
+++ vine/src/vine_turbo.py
@@ -218,8 +218,7 @@ class VINE_Turbo(torch.nn.Module, PyTorchModelHubMixin):
         B = x.shape[0]
         with torch.no_grad():
             stability_mask = self.stability_predictor(x)  # shape: (B, 1, H, W)
-        stability_mask = (stability_mask < 0.5).float()
-        stability_mask = 1.0 - stability_mask
+        stability_mask = (stability_mask < 0.01).float()
         stability_mask_3ch = stability_mask.repeat(1, 3, 1, 1)
 
         x_sec = self.sec_encoder(secret, x, stability_mask_3ch)
