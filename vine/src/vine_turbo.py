f4e66e2c8cb6e26d98ccf928a100c5df2840c164
jessieweneedtocook <J.Smith73@newcastle.ac.uk>
2025-04-15 20:39:23 +0100

    initial commit

--- vine/src/vine_turbo.py
+++ vine/src/vine_turbo.py
@@ -218,8 +218,7 @@ class VINE_Turbo(torch.nn.Module, PyTorchModelHubMixin):
         B = x.shape[0]
         with torch.no_grad():
             stability_mask = self.stability_predictor(x)  # shape: (B, 1, H, W)
-        stability_mask = (stability_mask < 0.1).float()
-        stability_mask = 1 - stability_mask
+        stability_mask = (stability_mask < 0.15).float()
         stability_mask_3ch = stability_mask.repeat(1, 3, 1, 1)
         print('% of stable image: ', (stability_mask_3ch.sum() / stability_mask_3ch.numel()))
 
