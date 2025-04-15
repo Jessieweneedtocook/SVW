123ba5ad7f367e72634f0b4060e3f4482872037f
jessieweneedtocook <J.Smith73@newcastle.ac.uk>
2025-04-15 02:48:28 +0100

    initial commit

--- vine/src/vine_turbo.py
+++ vine/src/vine_turbo.py
@@ -221,7 +221,7 @@ class VINE_Turbo(torch.nn.Module, PyTorchModelHubMixin):
         stability_mask = 1.0 - stability_mask
         stability_mask_3ch = stability_mask.repeat(1, 3, 1, 1)
 
-        x_sec = self.sec_encoder(secret, x, stability_mask_3ch)
+        x_sec = self.sec_encoder(secret, x, mask=stability_mask_3ch)
         x_sec_cleaned = x * (1 - stability_mask_3ch) + x_sec * stability_mask_3ch
         x_enc = self.vae_enc(x_sec_cleaned, direction="a2b").to(x.dtype)
         model_pred = self.unet(x_enc, self.timesteps, encoder_hidden_states=self.fixed_a2b_emb_base,).sample.to(x.dtype)
